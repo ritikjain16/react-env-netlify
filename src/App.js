132ed238b44e6f592bf25934cb4e92592682aa18
Ritik Jain <ritik9628@gmail.com>
2023-03-27 16:59:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>App {process.env.REACT_APP_KEY45} </div>;
+  return <div>Ap1p {process.env.REACT_APP_KEY45} </div>;
 };
 
 export default App;
